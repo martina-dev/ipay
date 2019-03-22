af479faebc137257550219340d552cbf6e431343
martina-dev <martina.devlang@gmail.com>
2019-03-22 13:10:30 +0300

    final changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express')
+const express = require('express');
 const CryptoJS = require("crypto-js");
+var axios = require("axios");
+var fs = require('fs');
+
 
 const app = express()
 const port = 3000
@@ -8,7 +11,7 @@ const hashKey = 'demoCHANGED'
 
 app.set('view engine', 'pug')
 
-let data = {live:"0", oid: "112", ttl:"900", tel:"3456780987", eml:"martina.devlang@gmail.com", vid:"demo", curr:"KES", cbk: callback, crl:"0"}
+let data = {live:"0", oid: "374", ttl:"910", tel:"34598680987", eml:"martina.devlang@gmail.com", vid:"demo", curr:"KES", cbk: callback, crl:"0"}
 
 var datastring = [];
 
@@ -24,7 +27,36 @@ console.log(datastring)
 var generatedhash = CryptoJS.HmacSHA1(datastring, hashKey).toString()
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/home', (req, res) => res.render('index', {title: "world", data, generatedhash}))
+app.get('/', async(req, res) => {
+    console.log(req.query)
+    fs.appendFile('response.log', JSON.stringify(req.query), function(err) {
+        if (err) throw err;
+    })
+
+    //VerifyPayment Function takes in the Vendor url, and parameter values as a data object
+    verifyPayment = async function(data) { 
+        var err;  
+        axios.post("https://www.ipayafrica.com/ipn/?vendor=", data)
+            .then((response, data) => {
+                data = response
+                //Check the status code to confirm whether the request was successful
+                console.log("Status Code: ",data.status)
+                if (data.status == 200){
+                    //Send success message to user if successful
+                    res.send("Hello There SUCCESS is upon Us")
+                    console.log("It worked")
+                } else {
+                    res.send("Hello There There was a whoopsie")   
+                }
+            })
+            .then((error, err) => {
+                err = error
+            })
+    }
+
+    verifyPayment({"id": req.query.id, "ivm": req.query.ivm,"qwh": req.query.qwh,"afd": req.query.afd,"poi": req.query.poi,"uyt": req.query.uyt,"ifd": req.query.ifd})
+    
+})
+app.get('/home', (req, res) => res.render('index', {title: "IPAY AFRICA", data, generatedhash}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
